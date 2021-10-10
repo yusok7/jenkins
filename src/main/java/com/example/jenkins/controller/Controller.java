fbade5d48e552ab3d0a344a6b9c53504e401464f
yusok7 <diddbtjr7@naver.com>
2021-10-10 16:50:42 +0900

    test

---
+++ src/main/java/com/example/jenkins/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hi";
+    }
+}
